54c9428fafbc4efa780efeda43ba4d85f0209ad7
Verdayne <daynegraham@yahoo.com>
2016-12-30 21:11:44 -0500

    FadeManager
    It works but not in between scenes. My functions to do it between scenes
    doesn't seem to work. Therefore calling it just before and after the
    scene is loaded seems to be a better option.
    
    //Fade out
    FadeManager.Fade(true, 1.25f or another float number)
    
    //Scene Load Script
    
    //Fade in
    FadeManager.Fade(false, 3f or another float number)

---
+++ Mind Control/Assets/Scripts/FadeManager.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts
+{
+    public class FadeManager : MonoBehaviour
+    {
+        public static FadeManager Instance { set; get; }
+        public Image FadeImage;
+        private float _duration;
+        private float _transition;
+        private bool _showing;
+        private bool _inTransition;
+
+        private void Awake()
+        {
+            Instance = this;
+            DontDestroyOnLoad(FadeImage);
+        }
+
+        public void Fade(bool showing, float duration)
+        {
+            _showing = showing;
+            _inTransition = true;
+            _duration = duration;
+            _transition = _showing ? 0 : 1;
+        }
+
+        //void OnEnable()
+        //{
+        //    SceneManager.sceneLoaded += OnLevelFinishedLoading;
+        //}
+
+        //void OnDisable()
+        //{
+        //    SceneManager.sceneLoaded -= OnLevelFinishedLoading;
+        //}
+
+        //void OnLevelFinishedLoading(Scene scene, LoadSceneMode mode)
+        //{
+        //    Debug.Log("Level Loaded");
+        //    Debug.Log(scene.name);
+        //    Debug.Log(mode);
+        //}
+
+        private void Update()
+        {
+            //  Test F1 - Fade out || F2 - Fade in
+            if (Input.GetKeyDown(KeyCode.F1)) Fade(true, 1.25f); 
+            if (Input.GetKeyDown(KeyCode.F2)) Fade(false, 3f);
+            //  Test End
+
+            if (!_inTransition) return;
+
+            _transition += (_showing) ? Time.deltaTime * (1 / _duration) : -Time.deltaTime * (1 / _duration);
+            FadeImage.color = Color.Lerp(new Color(1, 1, 1, 0), Color.white, _transition);
+            if (FadeImage.color.a < 0.001f)
+                FadeImage.enabled = false;
+
+            if (_transition > 1 || _transition < 0)
+                _inTransition = false;
+        }
+    }
+}
+
