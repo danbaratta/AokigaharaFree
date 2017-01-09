6a452c8dd6c2608017155145ed2ef06eab5ab2f5
Socrates Quintana <socratesq3@fullsail.edu>
2017-01-08 21:12:32 -0500

    Revert
    Revert

--- Mind Control/Assets/Scripts/Morgan.cs
+++ Mind Control/Assets/Scripts/Morgan.cs
@@ -1,82 +1,67 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Morgan : MonoBehaviour {
+public class Morgan : MonoBehaviour
+{
 
-	[SerializeField]
-	private float walkSpeed = 5f;
-	[SerializeField]
-	private float walkAccel = 1f;
+    private float walkSpeed = 5f;
+    private float walkAccel = 1f;
 
-	public float dashSpeed = 20f;
-	public float dashTimer = 0f;
+    public float dashSpeed = 20f;
+    public float dashTimer = 0f;
     public float ConstDashTimer;
     public bool Dash;
 
-	[SerializeField]
-	private float jumpAccel = 0.5f;
-	[SerializeField]
-	private float initJumpSpeed = 8.5f;
-	[SerializeField]
-	private float jumpTimer = 0f;
-	[SerializeField]
-	private float maxJumpTimer = 0.1f;
+    private float jumpAccel = 0.5f;
+    private float initJumpSpeed = 8.5f;
+    private float jumpTimer = 0f;
+    private float maxJumpTimer = 0.1f;
 
-	private SpriteRenderer morganSprite;
+    private SpriteRenderer morganSprite;
 
 
-	public float WalkSpeed()
-	{
-		return walkSpeed;
-	}
+    public float WalkSpeed()
+    {
+        return walkSpeed;
+    }
 
-	public float WalkAccel()
-	{
-		return walkAccel;
-	}
+    public float WalkAccel()
+    {
+        return walkAccel;
+    }
 
-	public float DashSpeed()
-	{
-		return dashSpeed;
-	}
+    public float DashSpeed()
+    {
+        return dashSpeed;
+    }
 
-	public float DashTimer()
-	{
-		return dashTimer;
-	}
+    public float DashTimer()
+    {
+        return dashTimer;
+    }
 
-	public float JumpAccel()
-	{
-		return jumpAccel;
-	}
+    public float JumpAccel()
+    {
+        return jumpAccel;
+    }
 
-	public float InitJumpSpeed()
-	{
-		return initJumpSpeed;
-	}
+    public float InitJumpSpeed()
+    {
+        return initJumpSpeed;
+    }
 
-	public float JumpTimer()
-	{
-		return jumpTimer;
-	}
+    public float JumpTimer()
+    {
+        return jumpTimer;
+    }
 
-	public float MaxJumpTimer()
-	{
-		return maxJumpTimer;
-	}
+    public float MaxJumpTimer()
+    {
+        return maxJumpTimer;
+    }
 
-	public SpriteRenderer MorganSprite()
-	{
-		return morganSprite;
-	}
-
-	public void IncrementJumpTimer()
-	{
-		jumpTimer += Time.deltaTime;
-	}
-
-	public void resetJumpTimer()
-	{
-		jumpTimer = 0;
-	}
+    public SpriteRenderer MorganSprite()
+    {
+        return morganSprite;
+    }
 }
